bfe6a25e0ccca90553357dcba64fbcf92eabc0ff
Froz1 <andreykoroka@gmail.com>
2019-11-07 12:39:53 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-chat-bot';
+
+  links = [
+    { path: '/main', label: 'Головна', active: 'button-active'},
+    { path: '/chat', label: 'Чат', active: 'button-active'}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularBotModule} from './angular-bot/angular-bot.module';
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularBotModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
